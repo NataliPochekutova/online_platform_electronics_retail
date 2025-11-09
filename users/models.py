e13e3dfde50a8f8f62680862cdd572a5e7990868
Natalia Pochekutova <natalipochekutova51@gmail.com>
2025-11-09 21:58:07 +0800

    настроено приложение users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователь"""
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone = PhoneNumberField(
+        verbose_name="Телефон",
+        null=True,
+        blank=True,
+        help_text="Введите номер телефона",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
